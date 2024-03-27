893e822950278853426c223752ac9df682137368
olaide <holaryidealabi@gmail.com>
2024-03-27 10:13:07 -0700

    add simple validation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,72 @@
 import "../css/Login.css";
 import { Buttons } from "../component/Hero";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [check, setCheck] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  // console.log({ email, password, check });
+  const navigate = useNavigate();
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleCheck(e) {
+    setCheck(e.target.checked);
+  }
+
+  function handleLogin() {
+    // checking if the input field is not empty
+    if (!email) {
+      return toast("Enter Email ", {
+        theme: "light",
+      });
+    } else if (!password) {
+      return toast("Enter Password", {
+        theme: "light",
+      });
+    }
+    // !email || !password ? toast.error(error.message) :
+
+    setIsSubmit(true);
+
+    const payload = { username: email, password, check };
+    // console.log({ payload });
+
+    fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    })
+      .then(async (response) => {
+        if (response.status !== 200) {
+          const data = await response.json();
+          throw data;
+        }
+        return response.json();
+      })
+      .then((response) => {
+        response && navigate(-1);
+      })
+
+      .catch((error) => {
+        toast.error(error.message);
+      })
+      .finally(() => {
+        setIsSubmit(false);
+      });
+  }
+
   return (
     <div>
       <div className="spotify-black">
@@ -38,6 +102,7 @@ export default function LoginPage() {
             placeholder="Email address or username"
             closed=""
             opened=""
+            onChange={handleEmail}
           >
             Email address or username
           </SimpleForm>
@@ -47,6 +112,7 @@ export default function LoginPage() {
             placeholder="Password"
             closed="../../assets/eye-slash.svg"
             opened="../../assets/eye.svg"
+            onChange={handlePassword}
           >
             Password
           </SimpleForm>
@@ -54,10 +120,12 @@ export default function LoginPage() {
         <a href="#/">Forget your passworld?</a>
         <div className="mini">
           <div>
-            <input type="checkbox" id="box" />
+            <input type="checkbox" id="box" onClick={handleCheck} />
             <label htmlFor="box">Remember me</label>
           </div>
-          <Buttons type="green-btn">Log In</Buttons>
+          <Buttons type="green-btn" login={handleLogin} disabled={isSubmit}>
+            Log In
+          </Buttons>
         </div>
 
         <hr />
@@ -68,7 +136,15 @@ export default function LoginPage() {
   );
 }
 
-function SimpleForm({ name, type, placeholder, closed, opened, children }) {
+function SimpleForm({
+  name,
+  type,
+  placeholder,
+  closed,
+  opened,
+  onChange,
+  children,
+}) {
   const [view, setView] = useState(false);
 
   function handleView() {
@@ -83,11 +159,12 @@ function SimpleForm({ name, type, placeholder, closed, opened, children }) {
           type={view ? "text" : type}
           placeholder={placeholder}
           id={name}
+          onChange={onChange}
         />
         <img
           src={view ? opened : closed}
           onClick={handleView}
-          alt=""
+          alt={opened || closed ? "eye" : ""}
           className="eye"
         />
       </div>
